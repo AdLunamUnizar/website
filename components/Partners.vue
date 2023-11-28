1313d86238f5850d72e607f00cccc63cf81b497b
berrujaime <71706688+berrujaime@users.noreply.github.com>
2023-11-28 02:31:36 +0100

    Partners

--- components/Partners.vue
+++ components/Partners.vue
@@ -1,5 +1,14 @@
 <template>
     <div class="flex flex-col w-full items-center justify-center p-3">
-        <h1>Partners</h1>
+        <h1 class="text-center">Partners</h1>
+    
+        <div class="flex flex-wrap justify-center">
+            <!-- Imágenes de partners -->
+            <a href="https://edu.ieee.org/es-unizar/" class="m-2"><img src="images/partners/IEEE.svg" class="w-52 h-auto" alt="IEEE" /></a>
+            <a href="https://www.clipcarbono.com/es/" class="m-2"><img src="images/partners/Clip_carbono.svg" class="w-52 h-auto" alt="Clip carbono" /></a>
+            <a href="https://didyf.unizar.es/" class="m-2"><img src="images/partners/Diseno_y_fabricacion.svg" class="w-52 h-auto" alt="Diseño y fabricación UNIZAR" /></a>
+            <a href="https://ctmyf.unizar.es/" class="m-2"><img src="images/partners/Materiales_y_fluidos.svg" class="w-52 h-auto" alt="Ciencia y tecnología de materiales y fluidos" /></a>
+        </div>
     </div>
 </template>
+  
