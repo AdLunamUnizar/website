d32da6aacd58f889d78c74f26d6e50698a6bf514
berrujaime <berrujaime@gmail.com>
2024-02-15 12:44:13 +0100

    partners

--- components/Partners.vue
+++ components/Partners.vue
@@ -1,13 +1,16 @@
 <template>
-    <div class="flex flex-col w-full items-center justify-center p-3">
-        <h1 class="text-center">Partners</h1>
-    
-        <div class="flex flex-wrap justify-center">
-            <!-- Imágenes de partners -->
-            <a href="https://edu.ieee.org/es-unizar/" class="m-2"><img src="images/partners/IEEE.svg" class="w-52 h-auto" alt="IEEE" /></a>
-            <a href="https://www.clipcarbono.com/es/" class="m-2"><img src="images/partners/Clip_carbono.svg" class="w-52 h-auto" alt="Clip carbono" /></a>
-            <a href="https://didyf.unizar.es/" class="m-2"><img src="images/partners/Diseno_y_fabricacion.svg" class="w-52 h-auto" alt="Diseño y fabricación UNIZAR" /></a>
-            <a href="https://ctmyf.unizar.es/" class="m-2"><img src="images/partners/Materiales_y_fluidos.svg" class="w-52 h-auto" alt="Ciencia y tecnología de materiales y fluidos" /></a>
+    <div class="flex flex-col items-center justify-center p-3 bg-slate-200">
+        <div class="max-w-2xl">
+            <h1 class="text-center text-black">Partners</h1>
+        
+            <div class="flex flex-wrap justify-center">
+                <!-- Imágenes de partners -->
+                <a href="https://edu.ieee.org/es-unizar/" class="m-2"><img src="images/partners/ieee.png" class="w-auto h-20 m-3" alt="IEEE" /></a>
+                <a href="https://www.clipcarbono.com/es/" class="m-2"><img src="images/partners/clip_carbono.jpg" class="w-auto h-20  m-3" alt="Clip carbono" /></a>
+                <a href="https://didyf.unizar.es/" class="m-2"><img src="images/partners/didyf.png" class="w-auto h-20  m-3" alt="Diseño y fabricación UNIZAR" /></a>
+                <a href="https://ctmyf.unizar.es/" class="m-2"><img src="images/partners/ctmyf.png" class="w-auto h-20  m-3" alt="Ciencia y tecnología de materiales y fluidos" /></a>
+                <a href="https://www.jointhe.space/" class="m-2"><img src="images/partners/jointhespace.png" class="w-auto h-20  m-3" alt="Join The Space" /></a>
+            </div>
         </div>
     </div>
 </template>
