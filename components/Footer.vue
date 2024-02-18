e32ef9af349d637bdd1f3e170641ea65b006ef47
berrujaime <berrujaime@gmail.com>
2024-02-18 17:49:45 +0100

    min height to blog page

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,7 +2,7 @@
 <!-- Footer -->
 <!-- Ref: https://tailwindcomponents.com/component/footer-2 -->
 <template>
-<section class="bg-black">
+<footer class="bg-black">
     <div class="max-w-screen-xl px-4 py-12 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8">
         <nav class="flex flex-wrap justify-center -mx-5 -my-2">
             <div class="px-5 py-2">
@@ -65,5 +65,5 @@
             © 2024 ALUnizar. All rights reserved.
         </p>
     </div>
-</section>
+</footer>
 </template>
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -2,7 +2,7 @@
     <Header></Header>
     <Main>
         <h1 class="text-center mt-5">Blog</h1>
-        <div class="w-full flex flex-wrap justify-center md:justify-between">
+        <div class="w-full flex flex-wrap justify-center md:justify-between min-h-screen">
             <article v-for="post in posts" class="max-w-sm w-full my-10 flex flex-col">
                 <div :style="`background-image: url('/images/blog/` + post.photo + `')`" class="w-full h-40 rounded-md bg-cover"></div>
                 <a :href="post._path" class="mt-2 hover:text-primary"><h2>{{ post.title }}</h2></a>
