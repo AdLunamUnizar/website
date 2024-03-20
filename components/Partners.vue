9c3b903e166a07d6a0f2d6e288e188d5df582f6f
ayubero <andreees.ys@gmail.com>
2024-03-20 12:26:13 +0100

    Deployment

--- components/Partners.vue
+++ components/Partners.vue
@@ -5,11 +5,11 @@
         
             <div class="flex flex-wrap justify-center">
                 <!-- Imágenes de partners -->
-                <a href="https://edu.ieee.org/es-unizar/" class="m-2"><img src="/images/partners/ieee.png" class="w-auto h-20 m-3" alt="IEEE" /></a>
-                <a href="https://www.clipcarbono.com/es/" class="m-2"><img src="/images/partners/clip_carbono.jpg" class="w-auto h-20 m-3" alt="Clip carbono" /></a>
-                <a href="https://didyf.unizar.es/" class="m-2"><img src="/images/partners/didyf.png" class="w-auto h-20  m-3" alt="Diseño y fabricación UNIZAR" /></a>
-                <a href="https://ctmyf.unizar.es/" class="m-2"><img src="/images/partners/ctmyf.png" class="w-auto h-20  m-3" alt="Ciencia y tecnología de materiales y fluidos" /></a>
-                <a href="https://www.jointhe.space/" class="m-2"><img src="/images/partners/jointhespace.png" class="w-auto h-20  m-3" alt="Join The Space" /></a>
+                <a href="https://edu.ieee.org/es-unizar/" class="m-2"><img src="/public/images/partners/ieee.png" class="w-auto h-20 m-3" alt="IEEE" /></a>
+                <a href="https://www.clipcarbono.com/es/" class="m-2"><img src="/public/images/partners/clip_carbono.jpg" class="w-auto h-20  m-3" alt="Clip carbono" /></a>
+                <a href="https://didyf.unizar.es/" class="m-2"><img src="/public/images/partners/didyf.png" class="w-auto h-20  m-3" alt="Diseño y fabricación UNIZAR" /></a>
+                <a href="https://ctmyf.unizar.es/" class="m-2"><img src="/public/images/partners/ctmyf.png" class="w-auto h-20  m-3" alt="Ciencia y tecnología de materiales y fluidos" /></a>
+                <a href="https://www.jointhe.space/" class="m-2"><img src="/public/images/partners/jointhespace.png" class="w-auto h-20  m-3" alt="Join The Space" /></a>
             </div>
         </div>
     </div>
