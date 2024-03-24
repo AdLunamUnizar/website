42ffb36043b9ca3497759ef12549335f510b4669
Andres <andreees.ys@gmail.com>
2024-03-24 12:48:12 +0100

    Title & Description

--- app.vue
+++ app.vue
@@ -1,4 +1,11 @@
 <template>
+  <Head>
+    <Title>ALUnizar</Title>
+    <Meta
+      name="description"
+      content="AdLunamUnizar Rocketry Team Website."
+    />
+  </Head>
   <NuxtPage/>
   <div style="position: relative; z-index: -1;">
   <NuxtParticles
