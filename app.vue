13690676d4154fc9f1d231a84d607e23382de250
Jaime Berruete <berrujaime@gmail.com>
2026-03-28 20:08:25 +0100

    Delete particles from blog articles

--- app.vue
+++ app.vue
@@ -7,9 +7,19 @@
     />
   </Head>
   <div class="relative isolate min-h-screen">
-    <ParticlesBackground />
+    <ParticlesBackground v-if="showParticles" />
     <div class="relative z-10">
       <NuxtPage />
     </div>
   </div>
 </template>
+
+<script setup>
+const route = useRoute()
+
+const normalizedPath = computed(() => route.path.replace(/\/+$/, '') || '/')
+
+const showParticles = computed(() => {
+  return !normalizedPath.value.startsWith('/blog/')
+})
+</script>
